ea774aa4da76e5ce1e10599a80bd5f5bd751f87c
Raeid Saqur <raeidsaqur@cs.toronto.edu>
2022-11-15 20:16:42 -0500

    .

--- unity/Assets/Scripts/BaseFPSAgentController.cs
+++ unity/Assets/Scripts/BaseFPSAgentController.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Utility;
 using UnityEngine;
